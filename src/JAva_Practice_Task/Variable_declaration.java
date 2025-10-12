a965d2d4f0fb355cd6334f070919ff8796a57450
KunalAyare <ayarekunal062@gmail.com>
2025-10-12 11:32:35 +0530

    5th October all the class practice labs have been added here

---
+++ src/JAva_Practice_Task/Variable_declaration.java
@@ -0,0 +1,20 @@
+package JAva_Practice_Task;
+
+public class Variable_declaration {
+    public static void main(String[] args) {
+
+        int a = 25;
+        System.out.println(a);
+
+        double b = 3.14;
+        System.out.println(b);
+
+        boolean c = true;
+        System.out.println(c);
+
+        char d = 'A';
+        System.out.println(d);
+
+        //Integer: 25 Double: 3.14 Boolean: true Character: A
+    }
+}
