c4fd927bcd1302da06af2597859e022470e907e1
KunalAyare <ayarekunal062@gmail.com>
2025-10-12 11:45:39 +0530

    Create a Java program that demonstrates various arithmetic and assignment operators.
    **Requirements:**
    - Use +, -, *, /, % operators
    - Demonstrate assignment operators (=, +=, -=, *=, /=)
    - Show the results of each operation

---
+++ src/JAva_Practice_Task/Arithmetic_assignment_operators.java
@@ -0,0 +1,34 @@
+package JAva_Practice_Task;
+
+public class Arithmetic_assignment_operators {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Arithmetic Operator");
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        System.out.println("\n");
+        System.out.println("\n");
+
+        System.out.println("assignment Operator");
+
+        System.out.println(a!=b);
+        System.out.println(a+=b);
+        System.out.println(a-=b);
+        System.out.println(a*=b);
+        System.out.println(a/=b);
+
+
+
+
+        //Addition: 15 Subtraction: 5 Multiplication: 50 Division: 2 Modulus: 0
+    }
+}
