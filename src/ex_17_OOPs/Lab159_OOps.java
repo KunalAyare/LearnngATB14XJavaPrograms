e3fd1e1b5af43e29e19beec59ca2743765ab51e2
KunalAyare <ayarekunal062@gmail.com>
2025-10-12 18:51:03 +0530

    11 and 12 th October class all programs

---
+++ src/ex_17_OOPs/Lab159_OOps.java
@@ -0,0 +1,17 @@
+package ex_17_OOPs;
+
+class Person1 {
+    String name;
+}
+public class Lab159_OOps {
+
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.name = "Somya";
+
+        Person p2 = new Person();
+        p2.name = "Yogesh";
+
+        new Person();
+    }
+}
